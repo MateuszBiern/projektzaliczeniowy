c4052ac98bb35691266f5b6baeca7146d1820115
MateuszBiern <136621812+MateuszBiern@users.noreply.github.com>
2025-05-07 22:22:18 +0200

    Delete angu/Restauracja directory

--- angu/Restauracja/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { FoundersComponent } from './components/founders/founders.component';
-
-export const routes: Routes = [
-  { path: '', component: FoundersComponent }, // Główna ścieżka
-  // Dodaj inne ścieżki jeśli potrzebujesz
-];
